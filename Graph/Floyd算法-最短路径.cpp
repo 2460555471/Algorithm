6cbf01b1473c78261abe7e5abdb178bd368a6347
houyi <2460555471@qq.com>
2017-09-06 14:18:32 +0800

    路径输出

--- Graph/Floyd算法-最短路径.cpp
+++ Graph/Floyd算法-最短路径.cpp
@@ -10,6 +10,24 @@ using namespace std;
 const int V = 4;
 const int INF = 99999;
 
+int path[V][V];//path为路径
+
+//二分的方法来输出路径
+void pathSolution(int i, int j)//注意队后一个节点实际上并没有输出
+{
+	int k;
+	if (path[i][j] == i)
+	{
+		cout << i << " ";
+	}
+	else
+	{
+		k = path[i][j];
+		pathSolution(i, k);
+		pathSolution(k, j);
+
+	}
+}
 void Floyd(int graph[][V])
 {
 	int dist[V][V];
@@ -18,6 +36,7 @@ void Floyd(int graph[][V])
 		for (int j = 0; j < V; j++)
 		{
 			dist[i][j] = graph[i][j];
+			path[i][j] = i;//初始化为最一条路径最开始的点
 		}
 	}
 
@@ -31,21 +50,28 @@ void Floyd(int graph[][V])
 				if (dist[i][j]>dist[i][k] + dist[k][j])
 				{
 					dist[i][j] = dist[i][k] + dist[k][j];
+					path[i][j] = k;//i和j之间多了一个k,迭代着i和k之间/k和j之间都会更新新的k
 				}
 
 			}
 		}
 	}
-
 	for (int i = 0; i < V; i++)
 	{
-		for (int j = 0; j < V; j++)
+		for (int j = 0; j < i; j++)
 		{
-			cout<<dist[i][j]<<" ";
+			cout << i << "----->" << j << ":    ";
+			pathSolution(i, j);
+			cout << j<<"       ";
+			cout<<dist[i][j];
+			cout << endl;
 		}
-		cout << endl;
+		
 	}
+
+	
 }
+
 
 
 int main()
@@ -53,9 +79,9 @@ int main()
 	int graph[V][V] =
 	{ 
 		{ 0, 5, INF, 10 },
-		{ INF, 0, 3, INF },
-		{ INF, INF, 0, 1 },
-		{ INF, INF, INF, 0 }
+		{ 5, 0, 3, INF },
+		{ INF, 3, 0, 1 },
+		{ 10, INF, 1, 0 }
 	};
 	Floyd(graph);
 
